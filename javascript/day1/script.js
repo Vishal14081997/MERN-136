538a7907bbdf1e66034d38102da4e2cbddddd997
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-21 09:10:01 +0530

    day1

---
+++ javascript/day1/script.js
@@ -0,0 +1,26 @@
+console.log("hy vishal singh");
+// document.write("hello")
+
+/* multi line comment */
+
+// let , var , const
+// variable 
+// data type 
+// operator
+// loops 
+// function
+// array methods
+// sting methods
+// Object
+// DOM
+// Event
+// fetch api
+
+var a = 10;
+var a =13;
+console.log(a);
+
+document.write(a)
+let b =10;
+b= 12;
+console.log(b);
