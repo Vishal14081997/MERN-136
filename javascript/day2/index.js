f7e4804c42abfcb36187252d9401e3f49292c3d6
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-22 09:04:17 +0530

    day2 code

---
+++ javascript/day2/index.js
@@ -0,0 +1,85 @@
+
+// var - globle scop  - redeclare , update(assing)
+// let - block scop - update
+// const - block scop update (x) , redeclare(x)
+
+//------------- var --------------------
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// var b = 30;
+// b = 40;
+// console.log(b);
+
+// {
+//     var name = "vishal"
+// }
+// console.log(name);
+
+// --------- let -----------------
+// let a = 19;
+// a= 30;
+// console.log(a);
+
+// {
+//     let age = 20;
+// }
+// console.log(age);
+
+// ---------- const ---------------
+
+// const a = 30;
+// a = 40;
+// console.log(a);
+
+// {
+//     const age = 20;
+// }
+// console.log(age);
+
+// -----------data type ------------
+
+// 1) ------primtive dataType----
+// a). number
+// let a = 20;
+// b). string
+// let name = "vishal"
+// c). boolean(true false)
+// d). null
+// let data = null;
+// e). undefiend
+// f). symbol
+// g). bigint
+// let num = 12345678907897n
+
+// 2) --------- non premtive dataType
+// a) Array b) object 
+
+// let arr = [1, 2, 3, 4, 5, 6, "vishal", true, null, undefined,]
+
+// const obj = {
+//     // key:value
+//     name: "vishal",
+//     age: 40
+// }
+// console.log(obj["name"]);
+// console.log(obj.age);
+
+// ------------- naming convesion------------
+// camel case -
+// const myName = "vishal"
+// snake case - 
+// const my_name  = "vishal"
+
+// let name = "vishal"
+// console.log("name", typeof name);
+
+// const obj = {
+//     name = "vishal ",
+//     age = 34,
+//     arr = ["vishal" , "rajesh " , "mohan"]
+// }
+
+
+
