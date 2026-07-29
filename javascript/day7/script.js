c7ddf9cb4df89011a9e9e5bda3dc7dcb9e6f3a88
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-29 20:29:47 +0530

    Assigment operator

---
+++ javascript/day7/script.js
@@ -0,0 +1,62 @@
+// operator - 
+
+//! Assigment operator
+
+// let firstVariable = "hello world"
+
+// let firstVariable = 20; 
+// let secondVariable = 10;
+// let sum = firstVariable * secondVariable 
+// let division = firstVariable / secondVariable
+// let exponential = secondVariable ** firstVariable
+// let modulus =  secondVariable % firstVariable
+// console.log(sum);
+// console.log(division);
+// console.log(exponential);
+// console.log(modulus);
+
+//! addition assigment operator 
+// let x = 10;
+// let y = 20;
+// console.log(x);
+// // x = x + y 
+// x += y
+// console.log(x);
+
+//! subtration assigment operator 
+
+// let x = 10;
+// let y = 20;
+// console.log(x);
+// // x = x + y 
+// x -= y
+// console.log(x);
+
+//! logical operators 
+//  &&  ||  !
+
+// let firstVariable = true;
+// let secondVariable = !firstVariable
+// console.log(secondVariable);
+
+
+// let firstVariable = -100;  
+// let secondVariable = !!firstVariable
+// console.log(secondVariable);
+
+// let firstVariable = "vishal";  
+// let secondVariable = !firstVariable
+// console.log(secondVariable);
+
+// let firstVariable = null;  
+// let secondVariable = !firstVariable
+// console.log(secondVariable);
+
+// let firstVariable = {};  
+// let secondVariable = !firstVariable
+// console.log(secondVariable);
+
+// let firstVariable = [];  
+// let secondVariable = !firstVariable
+// console.log(secondVariable);
+
