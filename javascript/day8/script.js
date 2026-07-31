1d7e3cf3e48ae5ebfd8b2c78f04c8726b78b5c62
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-31 20:59:40 +0530

    day9

---
+++ javascript/day8/script.js
@@ -0,0 +1,75 @@
+// --------- bitwise operator---------
+
+// &, | ,^, << >> ~
+
+// 1. bitwise And(&)
+
+// let a = 5; // 101
+// let b = 3; // 011
+// console.log(a & b);
+
+// 2. bitwise OR(|)
+
+// let a = 5; // 101
+// let b = 3; // 011
+
+// console.log(a | b)
+
+// 3.bitwise XOR(^)
+
+// let a = 5; // 101
+// let b = 3; // 011
+
+// console.log(a^b);
+
+// 4. left shift(<<)
+
+// console.log(5<<1);
+// mult x 2
+
+// 5. right shift(>>)
+
+// console.log(8>>1);
+// division by 2
+
+// 6. bitwise Notification(~)
+// console.log(~5); // -(5+1) = -6
+// console.log(~10);
+
+
+//------------ control flow statements -------------
+
+// canditional statements 
+// loops 
+
+// if-else
+// switch cases
+
+// let number = 30;
+// if (number >= 30) {
+//     console.log("the number is grater than 30");
+// }
+
+// let number = 30;
+// if(number % 2 === 0){
+//     console.log("the number is even");
+// }else{
+//     console.log("the number is odd"); 
+// }
+
+// let marks = 83;
+// if (marks <= 100 && marks >= 90) {
+//     console.log("Grade : A+");
+// } else if (marks < 90 && marks >= 80) {
+//     console.log("Grade : A");
+// } else if (marks < 80 && marks >= 70) {
+//     console.log("Grade : B");
+// } else if (marks < 70 && marks >= 60) {
+//     console.log("Grade : C");
+// } else if (marks < 60 && marks >= 50) {
+//     console.log("Grade : D");
+// } else if (marks < 50) {
+//     console.log("Grade : F");
+// } else {
+//     console.log("Invalid marks");
+// }
---
+++ javascript/day9/script.js
@@ -0,0 +1,84 @@
+// ------- Ternory operator-------
+
+// let marks = 101;
+
+// marks >= 90 ? console.log("Grade : A+ ") : console.log("Grade something");
+
+
+// marks <= 100 && marks >= 90 ? console.log("Grade : A+") : marks < 90 && marks >= 80 ? console.log("Grade : A") : marks < 80 && marks >= 70 ? console.log("Grade : B") : marks < 70 && marks >= 60 ? console.log("Grade : C") : marks < 60 && marks >= 50 ? console.log("Grade : D") : marks < 50 ? console.log("Grade : F") : console.log("Invalid marks");
+
+
+// ------- switch cases---------
+
+// let day = "Monday"
+
+// switch (day) {
+//     default:
+//         console.log("Invalid day");
+//         break;
+//     case "Monday":
+//         console.log("first day of the week");
+//         break;
+//     case "Tuesday":
+//         console.log("second day of the week");
+//         break;
+//     case "Wednesday":
+//         console.log("third day of the week");
+//         break;
+//     case "Thrusday":
+//         console.log("fourth day of the week");
+//         break;
+//     case "Friday":
+//         console.log("fifth day of the week");
+//         break;
+//     case "Saturday":
+//         console.log("six day of the week");
+//         break;
+//     case "Sunday":
+//         console.log("seven day of the week");
+//         break;
+// }
+
+
+let month = 13;
+
+switch (month) {
+    case 1 ,13:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+}
