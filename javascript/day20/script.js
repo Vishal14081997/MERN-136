f8d5bab8a7f6d74efdcc815f5dc1aa7112622a16
Vishal14081997 <1408vishalsingh@gmail.com>
2026-08-17 08:32:15 +0530

    event

---
+++ javascript/day20/script.js
@@ -0,0 +1,63 @@
+// const mydiv = document.getElementById("container")
+// console.log(mydiv);
+
+// let mydiv = document.getElementsByClassName('box')
+// console.log(mydiv[9]);
+
+// mydiv[4].style.background = "yellow"
+// mydiv[4].style.padding = "10px"
+
+// for (let i = 0; i < mydiv.length; i++) {
+//     if (i % 2 === 0) {
+//         mydiv[i].style.background = "green"
+//     } else {
+//         mydiv[i].style.background = "yellow"
+//     }
+//     mydiv[i].style.padding = "10px"
+//     mydiv[i].style.margin = "10px"
+// }
+
+// let color = ["green" , "brown","yellow" , "skyblue", "" ,"","","red"]
+
+// for(let i =0; i<mydiv.length; i++){
+//     mydiv[i].style.background = color[i];
+//     mydiv[i].style.padding = "10px"
+// }
+
+
+// querySelector
+// querySelectorAll
+
+// let mydiv = document.querySelector(".box")
+// console.log(mydiv);
+
+// let newdiv = document.querySelectorAll(".box")
+// console.log(newdiv);
+
+// newdiv.forEach((item)=>{
+//     item.style.background = "yellow"
+// })
+
+// newdiv.forEach((item) => {
+//     item.addEventListener("click", () => {
+//         item.style.background = "green"
+//         item.style.padding = "10px"
+//     })
+// })
+
+// let newdiv = document.createElement("div")
+// newdiv.id = "item"
+// newdiv.className = "box"
+// newdiv.style.padding = "10px"
+// newdiv.style.background = "red"
+
+// console.log(newdiv);
+
+// document.body.appendChild(newdiv)
+
+// let mydiv4th  = document.getElementById("item-4")
+// console.log(mydiv4th);
+
+// document.body.insertBefore(newdiv ,mydiv4th)
+
+// document.getElementById("container").appendChild(newdiv)
---
+++ javascript/day21/script.js
@@ -0,0 +1,17 @@
+// Events 
+
+// function sum(){
+//     console.log(10+20); 
+// }
+
+
+function sum() {
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+    console.log(Number(number1) + Number(number2));
+}
+function sub() {
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+    console.log(Number(number1) - Number(number2));
+}
