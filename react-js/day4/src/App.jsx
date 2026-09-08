dd4060a96c05c3bdb1b094f4926a81f3870211c8
Vishal14081997 <1408vishalsingh@gmail.com>
2026-09-08 08:33:25 +0530

    update code

--- react-js/day4/src/App.jsx
+++ react-js/day4/src/App.jsx
@@ -1,53 +1,121 @@
-import React from 'react'
-import Child from './components/Child'
-import ProductChild from './components/ProductChild'
+// import React, { useState } from 'react'
+// import Child from './components/Child'
+// import ProductChild from './components/ProductChild'
+
+// const App = () => {
+//   const [userData, setUserData] = useState({
+//     name: "vishal",
+//     age: "25",
+//     gender: "Male",
+//     study: "full stack devloper"
+//   })
+
+//   const changeData = () => {
+//     setUserData({
+//       name: "Rajesh",
+//       age: "30",
+//       gender: "Male",
+//       study: "Mern stack devloper"
+//     })
+//   }
+
+//   let productdata = [
+//     {
+//       id: 1,
+//       title: "Iphone",
+//       des: "this is iphone series of apple",
+//       price: 2000,
+//       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
+//     },
+//     {
+//       id: 2,
+//       title: "sumsung s24 ",
+//       des: "this is sumsung series ",
+//       price: 2000,
+//       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
+//     },
+//     {
+//       id: 3,
+//       title: "sumsung s24 ",
+//       des: "this is sumsung series ",
+//       price: 2000,
+//       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
+//     },
+//     {
+//       id: 4,
+//       title: "sumsung s24 ",
+//       des: "this is sumsung series ",
+//       price: 2000,
+//       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
+//     },
+
+//   ]
+
+//   const [data, setData] = useState(productdata)
+
+//   const changeProduct = (id) => {
+//     console.log(id);
+//     setData((oldData) => {
+//       // console.log(oldData);
+//       return oldData.map((item) => {
+//         // console.log(item);
+//         if (item.id === id) {
+//           return {
+//             ...item,
+//             title: "update data",
+//             des: "all product change",
+//             price: 6000,
+//             image: "https://m.media-amazon.com/images/I/71ju4kS-W8L._AC_UF1000,1000_QL80_.jpg"
+//           }
+//         }
+//         return item
+//       })
+//     })
+//   }
+
+//   return (
+//     <>
+//       {/* <Child heading="my name vishal singh" title="child1" /> */}
+//       <div className='flex gap-5 mt-10 justify-between p-4'>
+//         {
+//           data.map((item) => {
+//             return (
+//               <ProductChild key={item.id} productData={item} changeProduct={changeProduct} />
+//             )
+//           })
+//         }
+//       </div>
+
+//       <div className='bg-gray-400 inline-block ml-10 p-2 rounded-2xl'>
+//         <h1>{userData.name}</h1>
+//         <h1>{userData.age}</h1>
+//         <h1>{userData.gender}</h1>
+//         <h1>{userData.study}</h1>
+//         <button className='bg-blue-700 rounded-2xl  text-white p-2 mt-4' onClick={changeData}>Update data</button>
+//       </div>
+
+//     </>
+//   )
+// }
+// export default App
+
+
+// -------------- 2nd -------------------------
+
+import React, { useState } from 'react'
+import Button from "@mui/material/Button"
 
 const App = () => {
-
-  let data = [
-    {
-      id: 1,
-      title: "Iphone",
-      des: "this is iphone series of apple",
-      price: 2000,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
-    },
-    {
-      id: 2,
-      title: "sumsung s24 ",
-      des: "this is sumsung series ",
-      price: 2000,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
-    },
-    {
-      id: 3,
-      title: "sumsung s24 ",
-      des: "this is sumsung series ",
-      price: 2000,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
-    },
-    {
-      id: 4,
-      title: "sumsung s24 ",
-      des: "this is sumsung series ",
-      price: 2000,
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaE9GF2i2qAhIEsJGKfM3kROp5WaTotiBkLV8l32A3g&s"
-    },
-   
-  ]
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
   return (
     <>
-      {/* <Child heading="my name vishal singh" title="child1" /> */}
-      <div className='flex gap-5 mt-10 justify-between p-4'>
-      {
-        data.map((item) => {
-          return (
-            <ProductChild key={item.id} productData={item} />
-          )
-        })
-      }
+      <div>
+        <h1>Count:{count}</h1>
+        <Button variant="outlined" onClick={handleIncrement}>Increment</Button>
       </div>
-
     </>
   )
 }
--- react-js/day4/src/components/ProductChild.jsx
+++ react-js/day4/src/components/ProductChild.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const ProductChild = ({ productData }) => {
-    console.log(productData);
+const ProductChild = ({ productData, changeProduct }) => {
+    // console.log(productData);
     return (
         <>
             <div className='bg-gray-300 inline-block p-4 rounded-[30px] text-center '>
-                <img className='rounded-[30px]' src={productData.image} alt="" />
+                <img className='rounded-[30px]' src={productData.image} width={100} alt="" />
                 <h1>{productData.title}</h1>
                 <p>{productData.des}</p>
                 <p>{productData.price}</p>
-                <button className='bg-blue-600 text-[#fff] rounded-2xl p-[4px] mt-2'>click me</button>
+                <button onClick={() => changeProduct(productData.id)} className='bg-blue-600 text-[#fff] rounded-2xl p-[4px] mt-2'>click me</button>
             </div>
         </>
     )
